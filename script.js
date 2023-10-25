2850fe143012b40438cb070b39dcb8e338523cb2
bowserguy56 <92765680+falcolombardifan@users.noreply.github.com>
2023-10-25 23:55:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var audio;
 var progressBar;
 var currentTimeLabel;
 var durationLabel;
+var volumeSlider;
 
 function playAudio() {
   if (!audio) {
@@ -9,6 +10,7 @@ function playAudio() {
     progressBar = document.getElementById("progressBar");
     currentTimeLabel = document.getElementById("currentTime");
     durationLabel = document.getElementById("duration");
+    volumeSlider = document.getElementById("volume-slider"); // Get volume slider element
 
     audio.addEventListener("loadedmetadata", function() {
       progressBar.max = audio.duration;
@@ -16,9 +18,21 @@ function playAudio() {
       durationLabel.textContent = formatTime(audio.duration);
       audio.addEventListener("timeupdate", updateProgressBar);
     });
+
+    // Set initial volume to 1 (100%)
+    audio.volume = volumeSlider.value / 100;
+
+    // Update volume when slider value changes
+    volumeSlider.addEventListener('input', function() {
+      // Convert slider value to a range between 0 and 1
+      const volumeValue = parseFloat(this.value) / 100;
+      // Set the volume of the audio element
+      audio.volume = volumeValue;
+    });
   }
   audio.play();
 }
+
 
 function stopAudio() {
   if (audio) {
